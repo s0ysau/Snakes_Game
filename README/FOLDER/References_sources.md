55d15cd56356d926553c1e0e00954ef00d2809ea
jerricklc@gmail.com <jerricklc@gmail.com>
2022-09-12 19:13:36 -0400

    Lose conditions work

---
+++ README/FOLDER/References_sources.md
@@ -0,0 +1,7 @@
+1. 
+https://www.freecodecamp.org/news/how-to-build-a-snake-game-in-javascript/
+
+2.
+https://www.freecodecamp.org/news/javascript-keycode-list-keypress-event-key-codes/
+
+3.
--- js/game.js
+++ js/game.js
@@ -13,11 +13,12 @@ class options {
     }
 }
 
-const testingMode = new options (0.9, 500)
+const testingMode = new options (0.9, 300)
 
 let appleIndex = 0; // <--- position of the apple
 let snake = [2, 1, 0];
 let yAxis = 20; // <--- Vertical
+let xAxis = 1;
 let direction = 1; // <--- amount snake can move at an interval
 let score = 0;
 let snakeSpeedtime = 0 // <--- speed snake moves
@@ -57,25 +58,50 @@ function startGame (){
 
 function triggers () { //<--- While the snake moves, checks if it triggers lose conditions
     let squares = document.querySelectorAll('.gameboard div')
-    if (bitingItself(squares)){
+    // if (bitingItself(squares)){
+    //     alert('You bit yourself!')
+    //     popUp.style.display = 'block'
+    // } else if (hittingTheWall(squares)) {
+    //     alert('You Slithered into a wall!')
+    //     popUp.style.display = 'block'
+    // } else {
+    //     snakeMove(squares)
+    // }
+    if ((snake[0] % yAxis === 0 && direction === -xAxis ) || //<-- Left
+        (snake[0] - yAxis <= -1 && direction === -yAxis) || //<-- Top 
+        (snake[0] + yAxis >= yAxis * yAxis && direction === yAxis) || //<-- Bottom
+        (snake[0] % yAxis === yAxis - 1 && direction === xAxis)){ //<-- Right
+        alert('You slithered into a wall!')
+        clearInterval(interval)
+        popUp.style.display = 'block'
+    } else if ((squares[snake[0] + direction].classList.contains('snake'))){
         alert('You bit yourself!')
+        clearInterval(interval)
         popUp.style.display = 'block'
     } else {
         snakeMove(squares)
     }
 }
+// function bitingItself (squares){ 
+//     if ((squares[snake[0] + direction].classList.contains('snake'))){
+//         clearInterval(interval)
+//         return true
+//     } else {
+//         hittingTheWall(squares)
+//     }
+// }
 
-function bitingItself (squares){ 
-    if ((squares[snake[0] + direction].classList.contains('snake'))){
-        clearInterval(interval)
-        return true
-    } else {
-        return false
-    }
-}
-
-// function hittingTheWall () {
-
+// function hittingTheWall (squares) {
+//     if ((squares[snake[0]] - yAxis === 0 && direction === -xAxis ) || //<-- Left
+//     (squares[snake[0]] - yAxis === -1 && direction === -yAxis) || //<-- Top border
+//     (squares[snake[0]] + yAxis >= yAxis * yAxis && direction === +yAxis) || //<-- Bottom
+//     (squares[snake[0]] % yAxis === yAxis - 1 && direction === xAxis)) //<-- Right
+//     {
+//         clearInterval(interval)
+//         return true
+//     } else {
+//         return false
+//     }
 // }
 
 function snakeMove () {
@@ -94,7 +120,7 @@ function eatingApple (squares, tail){
         snake.push(tail)
         placingApples(squares)
         score++
-        scoreBoard.innerHTML = `<h2 class="score">Score: ${score}</h2>`;
+        scoreBoard.innerHTML = `<h2 class="score">Score: <span class="scoreNm">${score}</span></h2>`;
         // <= increase speed for every apple eaten => //
         clearInterval(interval)
         snakeSpeedtime = snakeSpeedtime * testingMode.increaseSpeed ;
@@ -117,20 +143,20 @@ function placingApples (squares) {
 window.addEventListener('keydown', (evt) => {
     console.log(evt)
     if (evt.code === 'ArrowRight' || evt.code === 'KeyD') {
-        direction = 1 // right
+        direction = xAxis // right
     } else if (evt.code === 'ArrowUp' || evt.code === 'KeyW') {
         direction = -yAxis // up
     } else if (evt.code === 'ArrowLeft' || evt.code === 'KeyA') {
-        direction = -1 // left
+        direction = -xAxis // left
     } else if (evt.code === 'ArrowDown' || evt.code === "KeyS") {
-        direction = + yAxis //down
+        direction = yAxis //down
     } 
 })
 
-up.addEventListener("click", () => (direction = -yAxis));
-bottom.addEventListener("click", () => (direction = +yAxis));
-left.addEventListener("click", () => (direction = -1));
-right.addEventListener("click", () => (direction = 1));
+// up.addEventListener("click", () => (direction = -yAxis));
+// bottom.addEventListener("click", () => (direction = +yAxis));
+// left.addEventListener("click", () => (direction = -1));
+// right.addEventListener("click", () => (direction = 1));
 
 playAgain.addEventListener('click', () => {
     replay()
