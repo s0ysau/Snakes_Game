bb5c802964780f4603e6c498939532c198f7837c
jerricklc@gmail.com <jerricklc@gmail.com>
2023-02-02 13:32:38 -0500

    added .gitignore

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname + 'main_menu/main_menu.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/main_menu/main_menu.html'))
 })
 
 app.listen(PORT, () => {
