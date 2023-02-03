769baa28ebe13a759e878a732ce28b83ac983bfb
jerricklc@gmail.com <jerricklc@gmail.com>
2023-02-03 18:50:40 -0500

    works on local

--- server.js
+++ server.js
@@ -3,18 +3,20 @@ const express = require('express')
 const favicon = require('serve-favicon')
 const path = require('path')
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 8123
 
 const app = express()
 
 app.use(express.static(path.join(__dirname)))
 
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
-app.use(express.static(path.join(__dirname, 'build')))
+// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
+// app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+
+
 
 app.listen(PORT, () => {
   console.log(`Looking for snakes in ${PORT}`)
