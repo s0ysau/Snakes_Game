9401c3a7c68373b2277972309201460c6998b708
jerricklc@gmail.com <jerricklc@gmail.com>
2023-02-13 14:56:25 -0500

    made styling changes in the main menu, more additions in the backend

---
+++ controllers/highScores.js
@@ -0,0 +1,16 @@
+const HighScore = require("../models/highScores")
+
+const dataController = {
+  //index
+  //delete
+  //update
+  //create
+  //edit
+  //show
+}
+
+
+
+
+
+module.exports = dataController
