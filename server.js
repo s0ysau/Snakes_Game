c13ae03d53209aab3c8709dc5f98284a8cd36a80
jerricklc@gmail.com <jerricklc@gmail.com>
2023-02-17 12:15:46 -0500

    revert to 769baa28ebe13a759e878a732ce28b83ac983bfb for heroku redeployment

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const express = require('express')
 const favicon = require('serve-favicon')
 const path = require('path')
 
-const PORT = process.env.PORT || 8123
+const PORT = process.env.PORT || 8080
 
 const app = express()
 
+<<<<<<< HEAD
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico' )))
 app.use(express.static(path.join(__dirname, 'public')))
+=======
+app.use(express.static(path.join(__dirname)))
+
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico' )))
+app.use(express.static(path.join(__dirname, 'build')))
+>>>>>>> parent of 769baa2 (works on local)
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
 })
 
 app.listen(PORT, () => {
